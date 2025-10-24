4d310eb5a4e72fd09a7c8ba1be21de77b4df4a95
yashpatel974 <patelyash18102@gmail.com>
2025-10-23 20:28:06 -0400

    UDP_almost_done

--- Client/ClientUDP.py
+++ Client/ClientUDP.py
@@ -2,7 +2,7 @@ import socket
 import os
 import time
 
-serverIP = "10.102.173.254"
+serverIP = "169.226.237.49"
 serverPort = 12000
 
 #SOCK_DGRAM is for UDP connections
@@ -15,8 +15,6 @@ while message != "quit":
 	
     if message[0:4] == "PUT ":
         clientSocket.sendto(message.encode('utf-8') + b'\n', (serverIP, serverPort))
-
-
 
         # home/desktop/.../client/filename
         fileName = message[4:].strip()
@@ -48,6 +46,7 @@ while message != "quit":
                     try:
                         ack, addr = clientSocket.recvfrom(1024)
                         if ack == b"ACK":
+                            #print(f"Sent chunk of {len(chunk)} bytes.")
                             break
                     except socket.timeout:
                         print("Timeout, resending chunk...")
@@ -56,7 +55,35 @@ while message != "quit":
         fin, addr = clientSocket.recvfrom(1024)
         if fin == b"FIN":
             print(f"File {fileName} sent successfully.")
-        
+    
+    elif message[0:4] == "GET ":
+        clientSocket.sendto(message.encode('utf-8') + b'\n', (serverIP, serverPort))
+
+        msg, addr = clientSocket.recvfrom(1024) # Receive file size
+        if msg.startswith(b"LEN:"):
+            total_bytes = int(msg[4:].decode())
+            # send ack for length
+            clientSocket.sendto(b"ACK_LEN", (serverIP, serverPort))
+            print(f"Receiving file of size {total_bytes} bytes from server...")
+
+            # step 2: receive the file in chunks
+            received  = 0
+            file_to_get = message[4:].strip()
+            local_name = "UDP_" + os.path.basename(file_to_get)
+            local_download_path = "UDP_downloads"+ "/" + local_name
+            os.makedirs(os.path.dirname(local_download_path), exist_ok=True)
+
+            with open(local_download_path, 'wb') as f:
+                while received < total_bytes:
+                    data, addr = clientSocket.recvfrom(1024) # buffer size is 1024 bytes
+                    f.write(data)
+                    received += len(data)
+                    clientSocket.sendto(b"ACK", (serverIP, serverPort)) # send ack for each chunk
+
+            # after file is received, send FIN
+            print("File received completely.")
+            clientSocket.sendto(b"FIN", (serverIP, serverPort))
+            print(f"File {local_name} received from server.")
     message = input()
     
 
--- Server/ServerUDP.py
+++ Server/ServerUDP.py
@@ -1,8 +1,10 @@
 import socket
 import os
 
+# asking for port number to listen on from user
+port = input("Enter port number to listen on: ")
+serverPort = int(port)
 
-serverPort = 12000
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # sockname = serverSocket.getsockname()
 # print(sockname)
@@ -40,7 +42,8 @@ while True:
             
             with open(path_local_file, 'wb') as f:
                 while received < total_bytes:
-                    data, clientAddr = serverSocket.recvfrom(1024 + 32) # buffer size is 1024 bytes
+                    #data, clientAddr = serverSocket.recvfrom(1024 + 32)
+                    data, clientAddr = serverSocket.recvfrom(1024) # buffer size is 1024 bytes
                     f.write(data)
                     received += len(data)
                     serverSocket.sendto(b"ACK", clientAddr) # send ack for each chunk
@@ -50,3 +53,42 @@ while True:
             serverSocket.sendto(b"FIN", clientAddr)
             print(f"File {local_name} received from client.")
     
+    elif filename[0:4] == "GET ":
+        file_to_send = filename[4:].strip()
+        local_name = os.path.basename(file_to_send)
+
+        file_size = os.path.getsize(local_name)
+        print(f"Sending file {local_name} of size {file_size} bytes to {clientAddr}...")
+
+        serverSocket.sendto(f"LEN:{file_size}".encode(), clientAddr) # Send file size first
+
+        msg, clientAddr = serverSocket.recvfrom(1024) # Wait for client ack
+        if msg != b"ACK_LEN":
+            print("No ACK from client, aborting...")
+            continue
+        elif msg == b"ACK_LEN":
+            print("ACK_LEN received from client, sending file...")
+        
+        # now send the file in chunks
+        with open(local_name, 'rb') as f:
+            while True:
+                chunk = f.read(1000) # Read 1000 bytes at a time
+                if not chunk:
+                    break
+
+                while True:
+                    serverSocket.sendto(chunk, clientAddr)
+                    try:
+                        ack, addr = serverSocket.recvfrom(1024)
+                        if ack == b"ACK":
+                            #print(f"Sent chunk of {len(chunk)} bytes.")
+                            break
+                    except socket.timeout:
+                        print("Timeout, resending chunk...")
+            
+        # after file is sent, wait for FIN
+        fin, addr = serverSocket.recvfrom(1024)
+        if fin == b"FIN":
+            print(f"File {file_to_send} sent successfully to {clientAddr}.")
+        else:
+            print("No FIN received, something went wrong.")
